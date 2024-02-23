24d7dd07f4f4e0ab1cb1e5357032659bcb4ddf83
Yohans Nascimento <yohans.dev@gmail.com>
2024-02-23 18:08:29 -0300

    feat: post publish request to text editor page

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, first } from 'rxjs';
 import { PostDetails } from '../interfaces/PostDetails';
 import { Post } from '../interfaces/Post';
+import { FormGroup } from '@angular/forms';
+import { PublishPost } from '../interfaces/PublishPost';
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +21,10 @@ export class PostService {
 
   getPostById(id: number): Observable<Post>{
     return this.httpClient.get<Post>(`${this.apiUrl}/${id}`);
+  }
+
+  publishPost(post: PublishPost): Observable<PublishPost>{
+    //console.log(post);
+    return this.httpClient.post<PublishPost>(`${this.apiUrl}`, post);
   }
 }
