3876267e6dc13c3fa5579138c20f1972bad090d7
Yohans Nascimento <yohans.dev@gmail.com>
2024-02-26 22:41:15 -0300

    delete: unused component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
-import { TextEditorComponent } from './components/text-editor/text-editor.component';
-import { PostCardComponent } from './components/post-card/post-card.component';
 import { PostPageComponent } from './views/post-page/post-page.component';
 import { TextEditorPageComponent } from './views/text-editor-page/text-editor-page.component';
 
