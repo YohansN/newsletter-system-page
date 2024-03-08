c33b2e519187b7cb8ebc3adb6e000123d8d1b5c6
Yohans Nascimento <yohans.dev@gmail.com>
2024-03-08 18:11:17 -0300

    feat: created and implemented navigation bar

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PostCardComponent } from '../../components/post-card/post-card.component';
 import { FilterChipsComponent } from '../../components/filter-chips/filter-chips.component';
+import { NavigationBarComponent } from '../../components/navigation-bar/navigation-bar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FilterChipsComponent, PostCardComponent],
+  imports: [FilterChipsComponent, NavigationBarComponent ,PostCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
