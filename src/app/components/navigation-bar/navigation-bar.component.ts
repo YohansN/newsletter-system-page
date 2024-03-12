8ac31adb1150134b5ba5537fb112c0591d902716
Yohans Nascimento <yohans.dev@gmail.com>
2024-03-12 15:59:02 -0300

    feat: implemented copy to clipboard button on nav bar

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {Clipboard} from '@angular/cdk/clipboard';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatTooltipModule],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.scss'
 })
 export class NavigationBarComponent {
+  pageUrl: string = '';
+  constructor(private clipBoard: Clipboard, private snackBar: MatSnackBar){ }
+  
+  ngOnInit(){
+    this.pageUrl = window.location.href;
+  }
+
+  copyUrlToClibBoard(){
+    this.clipBoard.copy(this.pageUrl);
+    this.openSnackBar();
+  }
+
+  private openSnackBar() {
+    this.snackBar.open("Link da página copiado!", 'Fechar' ,{duration: 4000});
+  }
 }
